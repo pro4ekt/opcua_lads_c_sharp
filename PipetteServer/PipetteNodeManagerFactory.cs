71736249b6c02da9f76b132011ccc3f8a7da78d6
pro4ekt <zhekaprobro@gmail.com>
2026-04-27 15:01:18 +0200

    Добавил пипетку и робо руку

---
+++ PipetteServer/PipetteNodeManagerFactory.cs
@@ -0,0 +1,198 @@
+﻿using Opc.Ua;
+using Opc.Ua.Server;
+using System.IO;
+using Opc.Ua.Export;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpcUa.Lads.Foundation.Server
+{
+    public class PipetteNodeManagerFactory : INodeManagerFactory
+    {
+        public INodeManager Create(IServerInternal server, ApplicationConfiguration configuration)
+        {
+            return new PipetteNodeManager(server, configuration);
+        }
+
+        public StringCollection NamespacesUris => ["http://lab.server/Pipette/"];
+    }
+
+    public class PipetteNodeManager : CustomNodeManager2
+    {
+        // Токен отмены задачи
+        private CancellationTokenSource _pipettingCts;
+
+        /// <summary>
+        /// Конструктор NodeManager-а для нашего устройства. 
+        /// </summary>
+        public PipetteNodeManager(IServerInternal server, ApplicationConfiguration configuration) 
+            : base(server, configuration, 
+                "http://opcfoundation.org/UA/DI/",
+                "http://opcfoundation.org/UA/AMB/",
+                "http://opcfoundation.org/UA/Machinery/",
+                "http://opcfoundation.org/UA/LADS/",
+                "http://lab.server/Pipette/")
+        {
+            SystemContext.NodeIdFactory = this; // Говорим контексту, что этот менеджер будет создавать NodeId
+            NamespaceUris =
+            [
+                "http://opcfoundation.org/UA/DI/",
+                "http://opcfoundation.org/UA/AMB/",
+                "http://opcfoundation.org/UA/Machinery/",
+                "http://opcfoundation.org/UA/LADS/",
+                "http://lab.server/Pipette/"
+            ];
+        }
+
+        /// <summary>
+        /// Этот метод вызывается ядром сервера при его старте. 
+        /// </summary>
+        public override void CreateAddressSpace(IDictionary<NodeId, IList<IReference>> externalReferences)
+        {
+            lock (Lock)
+            {
+                // Убеждаемся, что в словаре есть базовая папка ObjectsFolder
+                if (!externalReferences.TryGetValue(ObjectIds.ObjectsFolder, out IList<IReference> references))
+                {
+                    externalReferences[ObjectIds.ObjectsFolder] = references = new List<IReference>();
+                }
+
+                // Ассемблея, где лежат базовые xml файлы
+                var foundationAssembly = typeof(OpcUa.Lads.Foundation.Server.NodeManager).Assembly;
+
+                // Импортируем зависимости Opc.Ua (DI -> AMB -> Machinery -> LADS)
+                ImportXmlResource(externalReferences, foundationAssembly, "OpcUa.Lads.Foundation.Server.NodeSet.Opc.Ua.DI.NodeSet2.xml");
+                ImportXmlResource(externalReferences, foundationAssembly, "OpcUa.Lads.Foundation.Server.NodeSet.Opc.Ua.AMB.NodeSet2.xml");
+                ImportXmlResource(externalReferences, foundationAssembly, "OpcUa.Lads.Foundation.Server.NodeSet.Opc.Ua.Machinery.NodeSet2.xml");
+                ImportXmlResource(externalReferences, foundationAssembly, "OpcUa.Lads.Foundation.Server.NodeSet.Opc.Ua.LADS.NodeSet2.xml");
+
+                // Читаем структуру из XML файла Pipette.xml как встроенный ресурс сборки PipetteServer
+                var pipetteAssembly = typeof(PipetteNodeManager).Assembly;
+                ImportXmlResource(externalReferences, pipetteAssembly, "PipetteServer.Pipette.xml");
+
+                AddReverseReferences(externalReferences);
+
+                // Привязываем C# логику к методам и переменным
+                AttachLogicHandlers();
+            }
+        }
+
+        private void ImportXmlResource(IDictionary<NodeId, IList<IReference>> externalReferences, System.Reflection.Assembly assembly, string resourcePath)
+        {
+            using var stream = assembly.GetManifestResourceStream(resourcePath);
+            if (stream == null) 
+            {
+                throw new Exception($"Cannot find embedded resource: {resourcePath} in assembly {assembly.FullName}");
+            }
+            var nodeSet = UANodeSet.Read(stream);
+            foreach (var nameSpace in nodeSet.NamespaceUris)
+            {
+                SystemContext.NamespaceUris.GetIndexOrAppend(nameSpace);
+            }
+
+            var predefinedNodes = new NodeStateCollection();
+            nodeSet.Import(SystemContext, predefinedNodes);
+            
+            var toImportNodes = new List<NodeState>();
+            foreach (var node in predefinedNodes)
+            {
+                if (node is BaseTypeState state && state.SuperTypeId != null &&
+                    node.NodeId.NamespaceIndex == state.SuperTypeId.NamespaceIndex &&
+                    !PredefinedNodes.ContainsKey(state.SuperTypeId))
+                {
+                    toImportNodes.Add(node);
+                }
+                else
+                {
+                    AddPredefinedNode(SystemContext, node);
+                }
+            }
+
+            foreach (var node in toImportNodes)
+            {
+                AddPredefinedNode(SystemContext, node);
+            }
+        }
+
+        /// <summary>
+        /// В этом методе мы "оживляем" простое дерево объектов, привязывая делегаты (коллбеки)
+        /// к переменным и методам, которые были загружены из XML.
+        /// Без этого метода устройство было бы только для чтения (или методы просто ничего бы не делали).
+        /// </summary>
+        private void AttachLogicHandlers()
+        {
+            ushort ns = SystemContext.NamespaceUris.GetIndexOrAppend("http://lab.server/Pipette/");
+
+            // Находим метод StartProgram (в XML это i=7017)
+            if (FindPredefinedNode(new NodeId(7017u, ns), typeof(MethodState)) is MethodState startMethod)
+            {
+                startMethod.OnCallMethod = Method_OnCall;
+            }
+
+            // Находим переменную AssetId (в XML это i=6018)
+            if (FindPredefinedNode(new NodeId(6018u, ns), typeof(BaseVariableState)) is BaseVariableState assetIdVar)
+            {
+                assetIdVar.OnWriteValue = new NodeValueEventHandler(OnVariableWrite);
+            }
+        }
+
+        /// <summary>
+        /// Вызывается автоматически, когда клиент (مثلاً, UaExpert) изменяет значение переменной и отправляет его серверу.
+        /// </summary>
+        private ServiceResult OnVariableWrite(ISystemContext context, NodeState node, NumericRange indexRange, QualifiedName dataEncoding, ref object value, ref StatusCode statusCode, ref DateTime timestamp)
+        {
+            Console.WriteLine($"[Pipette Remote Control]: Variable '{node.BrowseName.Name}' updated to '{value}' by client.");
+            // Тут можно передать команду на "железо", чтобы пипетка сменила скорость
+            return StatusCodes.Good; // Подтверждаем клиенту, что запись прошла успешно
+        }
+
+        /// <summary>
+        /// Вызывается автоматически, когда клиент запускает RPC-метод на сервере.
+        /// </summary>
+        private ServiceResult Method_OnCall(ISystemContext context, MethodState method, IList<object> inputArguments, IList<object> outputArguments)
+        {
+            Console.WriteLine($"[Pipette Remote Control]: Execute Command => '{method.BrowseName.Name}'");
+
+            if (method.BrowseName.Name == "StartProgram")
+            {
+                StartPipettingTask();
+            }
+            else if (method.BrowseName.Name == "StopProgram" || method.BrowseName.Name == "AbortPipetting")
+            {
+                // Останавливаем фоновый процесс, если нажали Abort
+                _pipettingCts?.Cancel();
+                Console.WriteLine("[Pipette]: Pipetting manually aborted.");
+            }
+
+            return StatusCodes.Good;
+        }
+
+        /// <summary>
+        /// Фоновая задача (Thread), которая имитирует работу пипетки.
+        /// </summary>
+        private void StartPipettingTask()
+        {
+            // Отменяем предыдущую задачу, если она была запущена
+            _pipettingCts?.Cancel();
+            _pipettingCts = new CancellationTokenSource();
+            var token = _pipettingCts.Token;
+
+            // Запускаем асинхронно в фоне, чтобы не блокировать сетевой поток OPC UA
+            Task.Run(async () =>
+            {
+                try
+                {
+                    while (!token.IsCancellationRequested)
+                    {
+                        Console.WriteLine("[Pipette Process]: Pipetting in progress...");
+                        await Task.Delay(1000, token);
+                    }
+                }
+                catch (TaskCanceledException)
+                {
+                    // Задача была отменена токеном
+                }
+            }, token);
+        }
+    }
+}
